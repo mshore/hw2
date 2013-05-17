a38d41e6fcd6eeabf6eea30a32ea0639524f034c
Marc Leef <marc_leef@Marc-Leefs-MacBook-Pro-3.local>
2013-05-17 14:12:25 -0700

    Parts 1 and 2 done

--- shell.c
+++ shell.c
@@ -7,12 +7,156 @@
 
 /*
  * File: Shell.c 
- * Author: Marc Leef 
- * Date: 4/25/13
- * Execution of the custom shell for assignment one. User specifies a timeout variable,
- * and any process that runs longer than this specified timeout is killed by the snarky
- * shell. 
+ * Authors: Marc Leef, Matt Shore
+ * Date: 5/27/13
+ * Implementation of the fully featured shell for project 2. 
  */
+ 
+ 
+void parseArgs(char* a[], int args) {
+		int k;
+		int fence1, fence2 = 0;
+		int in, out = 0;
+		char *inp;
+		char *outp; 
+		char *pleft[50] = {};
+		char *pright[50] = {}; 
+		int leftSize, rightSize = 0;
+		inp = 0;
+		outp = 0;    
+		int p = 0;   
+		for(k = 0; k < args + 1; k++) {
+			if(a[k] != NULL) {
+                
+				if(*a[k] == '>') {
+					out = 1;
+					if(a[k+1] != NULL) {
+						if(!outp) {
+						outp = a[k+1];
+						fence1 = k;
+						}
+						else {
+							perror("Incorrect Use of Redirection");
+							exit(0);
+						}
+					}
+                	
+				}
+				
+				if(*a[k] == '<') {
+					in = 1;
+					if(a[k+1] != NULL) {
+						if(!inp) {
+							inp = a[k+1];
+							fence2 = k;
+						}
+						
+						else {
+							perror("Incorrect Use of Redirection");
+							exit(0);
+						}
+					}
+				}
+				
+				if(*a[k] == '|') {
+					int i;
+					for(i = 0; i < k; i++) {
+						pleft[i] = a[i];
+						leftSize++;
+					}
+					int w = 0;
+					for(i = k+1; i < args; i++) {
+						pright[w] = a[i];
+						rightSize++;
+						w++;
+					}
+					p = 1;
+				}
+				
+			}
+			
+			else {
+				break;
+			}
+		}
+		
+		if(!inp && outp && !p) {
+			for(fence1 = fence1; fence1 < args; fence1++) {
+				a[fence1] = 0;
+			}
+		}
+		
+		if(inp && !outp && !p) {
+			a[fence2] = a[fence2+1];
+			a[fence2+1] = 0;
+		}
+		
+		if(inp && outp && !p) {
+			if(fence2 < fence1) {
+				for(fence1 = fence1; fence1 < args; fence1++) {
+					a[fence1] = 0;
+				}
+				a[fence2] = a[fence2+1];
+				a[fence2+1] = 0;
+			}
+			
+			else {
+				a[fence1] = a[fence2 + 1];
+				a[fence2 + 1] = 0;
+				for(fence1 = fence1+1; fence1 < fence2 + 1; fence1++) {
+					a[fence1] = 0;
+				}
+			
+			}
+			
+		}
+
+/*		
+			if (in)
+			{
+				int fd0 = open(inp, O_WRONLY | O_CREAT | O_TRUNC, 0);
+				dup2(fd0, STDIN_FILENO);
+				close(fd0);
+				in = 0;
+			}
+*/
+			
+					
+			if (out)
+			{
+				int fd1 = creat(outp, 0644);
+				if(fd1 < 0) {
+					perror("");
+				}
+				dup2(fd1, STDOUT_FILENO);
+				close(fd1);
+				out = 0;
+			}
+
+		
+			if(p) {
+				int pipefd[2];
+				pipe(pipefd);
+				int pid2 = fork();
+				
+				if (pid2 == 0){
+					dup2(pipefd[1], 1);
+					close(pipefd[0]);
+					parseArgs(pleft, leftSize);
+					setpgid (pid2, 0);
+					execvp(pleft[0], pleft);	
+				}
+			
+				else {
+						dup2(pipefd[0], 0);
+						close(pipefd[1]);
+						parseArgs(pright, rightSize);
+						setpgid (pid2, 0);
+						execvp(pright[0], pright);
+				}
+
+			}
+}
 
 char *my_strcpy(char* dest, char* source)
 {
@@ -68,112 +212,13 @@ int main(int argc, char *argv[], char *envp[]) {
 			argCount = argCount + 1;
 		}
 		
-		args[argCount] = (char *)0;
-		
-		int k;
-		int fence1 = 0;
-		int in, out = 0;
-		char *inp;
-		char *outp; 
-		char *pleft;
-		char *pright; 
-		inp = 0;
-		outp = 0;    
-		int p = 0;   
-		for(k = 0; k < argCount + 1; k++) {
-			if(args[k] != NULL) {
-                
-				if(*args[k] == '>') {
-					out = 1;
-					if(args[k+1] != NULL) {
-						outp = args[k+1];
-						fence1 = k;
-					}
-                	
-				}
-				
-				if(*args[k] == '<') {
-					in = 1;
-					if(args[k-1] != NULL) {
-						inp = args[k-1];
-						args[k] = args[k+1];
-						args[k+1] = 0;
-					}
-				}
-				
-				if(*args[k] == '|') {
-					pleft = args[k-1];
-					pright = args[k+1];
-					p = 1;
-				}
-				
-			}
-			
-			else {
-				break;
-			}
-		}
-		
-		if(!inp && outp) {
-			for(fence1 = fence1; fence1 < argCount; fence1++) {
-				args[fence1] = 0;
-			}
-		}
-		
-		
-		
-		
-		
-		
-		
-		
+		args[argCount] = (char *)0;		
 		
 		free_tokenizer( tokenizer ); /* free memory */
 		
 		pid = fork();
 		if(!pid) {
-			
-			if (in)
-			{
-				int fd0 = open(inp, O_WRONLY | O_CREAT | O_TRUNC, 0);
-				dup2(fd0, STDIN_FILENO);
-				close(fd0);
-				in = 0;
-			}
-			
-			if (out)
-			{
-				int fd1 = creat(outp, 0644);
-				dup2(fd1, STDOUT_FILENO);
-				close(fd1);
-				out = 0;
-			}
-			
-			if(p) {
-				int pipefd[2];
-				pipe(pipefd);
-				int pid2 = fork();
-				
-				if (pid2 == 0){
-					dup2(pipefd[0], 0);
-					close(pipefd[1]);
-					char *rightArgs[] = {pright, NULL};
-					execvp(pright, rightArgs);
-				}
-			
-				else {
-					dup2(pipefd[1], 1);
-					close(pipefd[0]);
-					char* leftArgs[] = {pleft, NULL};
-					execvp(pleft, leftArgs);
-				}
-			}
-
-
-
-			
-			
-			
+			parseArgs(args, argCount);
 			execvp(args[0],args);
 		}
 		
